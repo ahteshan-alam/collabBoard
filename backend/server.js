2e048bd6fc281374f181e640226d15103374b8cb
Ahteshan <mdahteshan796@gmail.com>
2026-08-30 18:19:46 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -73,6 +73,6 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
